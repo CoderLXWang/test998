eb07b16cfd23670c408a704bf22825531784e717
likang <likang@wealthgrow.cn>
2021-01-20 11:00:23 +0800

    no message

---
+++ TemplateApi.h
@@ -0,0 +1,23 @@
+//
+//  TemplateApi.h
+//  tanpuyun
+//
+//  Created by wealthgrow on 2020/6/18.
+//  Copyright © 2020 qimeng. All rights reserved.
+//
+
+#import "SJBaseRequest.h"
+
+@interface TemplateApi : SJBaseRequest<SJBaseRequestDelegate, SJBaseRequestParamDelegate, SJBaseRequestHeaderDelegate>
+
+/** 是否为读取新数据(对应下拉刷新) */
+@property (nonatomic,assign) BOOL isLoadNew;
+/** 是否为最后一页 */
+@property (nonatomic,assign) BOOL isLastPage;
+
+@property (nonatomic, copy) NSArray<<#ModelClass#> *> *dataArray;
+
+@property (nonatomic, <#p#>) <#ModelClass#> *<#name#>;
+
+@end
+
